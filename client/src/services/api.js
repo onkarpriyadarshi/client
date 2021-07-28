fade0d560b2dd4b45b8a6e285d7e6bb283e6758f
onkarpriyadarshi <66131221+onkarpriyadarshi@users.noreply.github.com>
2021-07-28 17:50:17 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = 'http://localhost:4000/api';
+const host = 'http://https://vote-serve.herokuapp.com//api';
 
 export const setToken = token => {
   if (token) {
